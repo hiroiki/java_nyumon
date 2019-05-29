75a8e93d38fe44842d08663a8040f551678b7a6e
Hiroki Nagashima <35445320+hiroiki@users.noreply.github.com>
2019-05-29 12:28:02 +0900

    Create Main552.java

---
+++ Main552.java
@@ -0,0 +1,11 @@
+public class Main552{
+	public static void main(String[] args){
+    add(100,"aaa");
+    add(200,"bbb");
+	  
+	}
+	//変数の型と引数の型を合わせる。(int x,String yを入れ替えるとだめ)
+	public static void add(int x,String y){
+	  System.out.println(x+y);
+	}
+}
