2136488efd120a5f905cbb6be7294da4aadace3e
Hiroki Nagashima <35445320+hiroiki@users.noreply.github.com>
2019-05-29 15:00:15 +0900

    Create Ren51.java

---
+++ Ren51.java
@@ -0,0 +1,12 @@
+class Ren51{
+  public static void introduceOneself(String name,int age,double height,char sex){
+    System.out.printf("私の名前は%sです\n",name);
+    System.out.printf("私の年齢は%dです\n",age);
+    System.out.printf("私の名前は%fです\n",height);
+    System.out.printf("私の名前は%sです\n",sex);
+    
+  }
+  public static void main(String[] args){
+    introduceOneself("富士山",50,174.3,'m');
+  }
+}
