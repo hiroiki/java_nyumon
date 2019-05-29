2b60dcac990c625a994eb4e807826c0a5d37d494
Hiroki Nagashima <35445320+hiroiki@users.noreply.github.com>
2019-05-29 10:50:17 +0900

    Update Main47.java

--- Main47.java
+++ Main47.java
@@ -1,6 +1,6 @@
 public class Main47{
 	public static void main(String[] args){
     int[] score = new int[5];
-    System.out.println(score[0]);
+    System.out.println(score[0]); //すべて0で初期化される。文字列の場合、nullで初期化される。
 	}
 }
