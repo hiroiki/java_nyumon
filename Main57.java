6d86445df35bdd5ed2f3518e38c034ac94d63861
Hiroki Nagashima <35445320+hiroiki@users.noreply.github.com>
2019-05-29 12:41:26 +0900

    Create Main57.java

---
+++ Main57.java
@@ -0,0 +1,12 @@
+public class Main57{
+	public static void main(String[] args){
+    int a = 100;
+    int b = 10;
+    int ans = add(a,b);
+    System.out.printf("%d + %d = %d",a,b,ans);
+ 	}
+	public static int add(int x,int y){
+	  int ans = x +y;
+	  return ans;
+	}
+}
