0bb4ba52d05180f2a6857a3f6843e50fbacd20dc
Hiroki Nagashima <35445320+hiroiki@users.noreply.github.com>
2019-05-29 12:23:09 +0900

    Create Main55.java

---
+++ Main55.java
@@ -0,0 +1,11 @@
+public class Main55{
+	public static void main(String[] args){
+    add(100,200);
+    add(200,50);
+	  
+	}
+	public static void add(int x,int y){
+	  int ans = x + y;
+	  System.out.printf("%d+%d=%d\n",x,y,ans);
+	}
+}
